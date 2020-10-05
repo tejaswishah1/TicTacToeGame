ac04683f787a3990ff23df205591e4101ea20e17
tejaswishah1 <tejaswishah1998@gmail.com>
2020-10-05 14:36:28 +0530

    [tejas] add Created board

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TicTocGameClass Game = new TicTocGameClass();
+            Game.CreateBoard();
+        }
+    }
+}
---
+++ TicTocGameClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    class TicTocGameClass
+    {
+        
+
+        public void CreateBoard()
+        {
+          
+            char[] GameBoard = new char[10];
+
+            for(int i=1; i<10; i++)
+            {
+                GameBoard[i] = ' ';
+            }
+            
+        }
+    }
+}
